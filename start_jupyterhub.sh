4db52207d91b58e398f4bfc11bedff71f3e1ac5a
chreberle <44467153+chreberle@users.noreply.github.com>
2019-06-17 18:00:54 +0200

    Update start_jupyterhub.sh

--- start_jupyterhub.sh
+++ start_jupyterhub.sh
@@ -1,7 +1,25 @@
 # Bash script to launch Jupyterhub on Kubernetes cluster
 
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+helm repo update
 
+RELEASE=jhub
+NAMESPACE=jhub
 
+helm upgrade --install $RELEASE jupyterhub/jupyterhub \
+  --namespace $NAMESPACE  \
+  --version=0.8.2 \
+  --values config.yaml
+
+# Auto completion
+# kubectl config set-context $(kubectl config current-context) --namespace ${NAMESPACE:-jhub}
 
 # Nice to know
-kubectl --namespace=jhub get svc proxy-public
+
+# Get IP etc
+#kubectl --namespace=jhub get svc proxy-public
+
+# Get pods
+# kubectl get pod --namespace jhub
+# kubectl get service --namespace jhub
+
